cd16a358d544b34efbd4833ff731d852a00dd01b
carlospinheirof <carlos.pinheiro.f@gmail.com>
2016-10-01 10:43:17 -0300

    testes de uma pilha usando cunit

---
+++ Cunit_stack.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "stack.h"
+#include <CUnit/CUnit.h>
+#include <CUnit/Basic.h>
+Stack *stack;
+void stack_test() 
+{
+  // Check if first param matches with second[2]
+	push(stack, 1);
+	push(stack, 2);
+	push(stack, 3);
+	push(stack, 4);
+  CU_ASSERT_EQUAL(peek(stack), 4);
+  CU_ASSERT_EQUAL(pop(stack), 3); // teste que vai dar errado
+  CU_ASSERT_EQUAL(pop(stack), 3);
+  CU_ASSERT_EQUAL(pop(stack), 2);
+}
+int main()
+{
+	stack = create_stack();
+	//iniciar o CUnit é necessário sempre iniciar antes de qualquer coisa com o CUnit
+	if (CUE_SUCCESS != CU_initialize_registry()) return CU_get_error();
+
+	CU_pSuite pSuite = NULL; // criando um suite para guardar o tipo de teste.
+	pSuite = CU_add_suite("stack_test_suite", 0, 0); //adicionando o suite.
+	if (NULL == pSuite) { //essa chave é para o victor :D
+		// isso aqui está testando se o suite foi adicionado/criado corretamente
+ 		 CU_cleanup_registry(); /*Só para falar logo isso aqui é para terminar o Cunit, como ja diz pagar 
+ 		 os registros.*/
+  		return CU_get_error();
+	}
+	// Add the test to the suite
+	if (NULL == CU_add_test(pSuite, "stack_test", stack_test)) 
+	{
+  		CU_cleanup_registry();
+  		return CU_get_error();
+	}
+	//setar os modos básicos de operação CU_BRM_VERBOSE é para mostrar o máximo de detalhes da execução
+	CU_basic_set_mode(CU_BRM_VERBOSE); 
+	// roda os testes e mostra os detalhes da exercução
+   	CU_basic_run_tests();
+   return CU_get_error();
+}
---
+++ stack.h
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <stdlib.h>
+/* Estrutura de dados pilha com array Autor Carlos Pinheiro */
+
+struct _Stack 
+{
+	int index;
+	int st[250];
+};
+typedef struct _Stack Stack;
+
+//Cria uma pilha vazia com no maximo 250 itens
+Stack* create_stack();
+//coloca um item na pilha
+void push(Stack *stack, int item);
+//tira o item da pilha se vazia returna -987984
+int pop(Stack *stack);
+//da uma olhada no primeiro item da pilha se vazia returna -987984
+int peek(Stack *stack);
+// verifica se a pilha está vazia
+int is_empty(Stack *stack);
+
+int is_empty(Stack *stack)
+{
+	return(stack != NULL && stack->index <= 0);
+}
+
+Stack* create_stack()
+{
+	Stack *newStack = (Stack*) malloc(sizeof(Stack));
+	newStack->index = -1;
+	return newStack;
+}
+void push(Stack *stack, int item)
+{
+	if(stack->index == 249)
+	{
+		printf("It's full,sorry\n");
+		return;
+	}	
+	stack->st[++stack->index] = item;
+	return;
+}
+
+int peek(Stack *stack)
+{
+	if(!is_empty(stack)) return stack->st[stack->index];
+	else
+	{
+		printf("está vazia\n");
+		return;
+	}
+}
+int pop(Stack *stack)
+{
+	if(!is_empty(stack)) return stack->st[stack->index--];
+	else
+	{
+		printf("está vazia\n");
+		return -987984;
+	}
+}
